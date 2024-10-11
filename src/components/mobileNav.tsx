810524e6426bf84e1a0d4a78cc225106840fe2b3
Alishba Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-10-11 19:05:22 +0500

    Personal-Portfolio

--- src/components/mobileNav.tsx
+++ src/components/mobileNav.tsx
@@ -3,56 +3,49 @@
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {CiMenuFries} from "react-icons/ci"
-
+import { CiMenuFries } from "react-icons/ci";
+import classNames from 'classnames'; // Import classnames for conditional classes
 
 const links = [
-    {
-        name: "home",
-        path: "/"
-    },
-    {
-        name: "services",
-        path: "/service"
-    },
-    {
-        name: "work",
-        path: "/work"
-    },
-    {
-        name: "resume",
-        path: "/resume"
-    },
-    {
-        name: "contact",
-        path: "/contact" 
-    }
-]
+  { name: "home", path: "/" },
+  { name: "services", path: "/service" },
+  { name: "work", path: "/work" },
+  { name: "resume", path: "/resume" },
+  { name: "contact", path: "/contact" },
+];
 
 export default function MobileNav() {
-    const pathname = usePathname();
+  const pathname = usePathname();
+  
   return (
-   <Sheet>
-    <SheetTrigger className="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-accent"/>
-        <SheetContent className="flex flex-col">
-            <div className="mt-32 mb-40 text-center text-2xl">
-            <Link href="/">
-            <h1>Alishba's</h1></Link>
-            </div>
-            <nav className="flex flex-col justify-center items-center gap-8">
-            {links.map((link, index) => {
-            return(
-                <Link 
-                href={link.path} 
-                key={index} 
-                className={`${link.path === pathname && "text-accent border-b-2 border-accent" } text-xl capitalize hover:text-accent transition-all`}> 
-                {link.name} </Link>
-            )
-        })}
-            </nav>
-        </SheetContent>
-    </SheetTrigger>
-   </Sheet>
-  )
+    <Sheet>
+      <SheetTrigger asChild>
+        <button className="flex justify-center items-center">
+          <CiMenuFries className="text-[32px] text-accent"/>
+        </button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1>Alishba's</h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => (
+            <Link 
+              href={link.path} 
+              key={index} 
+              className={classNames(
+                link.path === pathname && "text-accent border-b-2 border-accent",
+                "text-xl capitalize hover:text-accent transition-all"
+              )}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 }
+
